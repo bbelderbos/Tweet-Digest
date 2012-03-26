b734f5a3d7c4fa4ef6b45fd6b6657c39809d8960
Bob Belderbos <bobbelderbos@gmail.com>
2012-03-27 00:02:32 +0200

    removed experiment dir from source control

--- experiment/table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `stats` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user` varchar(50) COLLATE utf8_bin NOT NULL,
-  `numTweets` varchar(50) COLLATE utf8_bin NOT NULL,
-  `insertDate` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
